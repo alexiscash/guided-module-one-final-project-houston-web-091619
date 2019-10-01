09aeb134ab4773b52d2fd4d8549ad947cc14e713
alexiscash <alexiscash97@rocketmail.com>
2019-10-01 11:13:06 -0500

    basic functionality DONE

--- bin/run.rb
+++ bin/run.rb
@@ -1,53 +1,21 @@
 require_relative '../config/environment.rb'
 
+def run
+    loop do 
+        prompt = TTY::Prompt.new
+        answer = prompt.select('tf you want', %w(students classrooms subjects quit))
+        if answer == 'students'
+            p Student.all
+        elsif answer == 'classrooms'
+            p Classroom.all
+        elsif answer == 'subjects'
+            p Subject.all
+        end
+        break if answer == 'quit'
+    end
+end
 
 
-# data = YAML.load(File.open('/Users/flatironstudent/Desktop/projects/guided-module-one-final-project-houston-web-091619/bin/classes.yml'))
-# core_classes_array = data['subjects']['core_classes']
-# electives_array = data['subjects']['electives']
-# students_array = data['students']
-# professors_array = data['professors']
-
-# puts core_classes_array
-# puts
-# puts electives_array
-# puts
-# puts students_array
-# puts 
-# puts professors_array
-
-
-
-# def run
-#     prompt = TTY::Prompt.new
-#     answer = prompt.select('tf you want?', %w(students classrooms subjects))
-#     if answer == 'students'
-#         p Student.all 
-#     elsif answer == 'classrooms'
-#         p Classroom.all
-#     elsif answer == 'subjects'
-#         p Subject.all
-#     end
-# end
-
-# def run
-#     loop do 
-#         prompt = TTY::Prompt.new
-#         answer = prompt.select('tf you want', %w(students classrooms subjects quit))
-#         if answer == 'students'
-#             p Student.all
-#         elsif answer == 'classrooms'
-#             p Classroom.all
-#         elsif answer == 'subjects'
-#             p Subject.all
-#         end
-#         break if answer == 'quit'
-#     end
-# end
-
-# def print_info()
-
-# end
 
 
 
